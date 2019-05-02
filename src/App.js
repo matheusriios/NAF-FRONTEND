b2269861cab251f07a449e0ebac5e88406248e4b
matheus.rios <matheus.rios@zrp.com.br>
2019-05-01 23:22:34 -0300

    create page cadastrar cliente

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 //Components
-import Login from './components/Login'
-
+import CadastroCliente from './components/CadastroCliente'
 
 class App extends Component {
   constructor(props){
@@ -14,35 +13,10 @@ class App extends Component {
     }
   }
 
-  componentDidMount() {
-    this.reqApi()
-  }
-
-  reqApi = async () => {
-    var formData = new FormData();
-    formData.append('grant_type', 'password')
-    formData.append('client_id', '1')
-    formData.append('client_secret', 'KREUhtWzcoPp8W4lXKl1esjuyqn3JettR5TLY5UX')
-    formData.append('username', 'gerente@outlook.com')
-    formData.append('password', 'secret')
-    formData.append('scope', '' )
-      
-    const resp = await fetch('http://localhost:8000/oauth/token', {
-      method: 'POST',
-      body: formData,
-      headers: {
-        // 'Content-Type': 'application',
-        // 'Accept': 'application/json',        
-      }
-    });
-    const data = await resp.json()
-    console.log(data)
-  }
-
   render() {
     return (
       <div className="App">
-        <Login />
+        <CadastroCliente />
       </div>
     );
   }
